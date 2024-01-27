5fe997944b1cce2e13f6318ba7267f7ceb789fff
Althaf <128457629+Althafazu@users.noreply.github.com>
2024-01-27 20:00:28 +0700

    fix repetition

--- c/repetition/repetition.c
+++ c/repetition/repetition.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// procedur declaration
+void program_no_1 (void);
+void program_no_2 (void);
+void program_no_3 (void);
+void program_no_4 (void);
+void program_no_5 (void);
+void program_no_6 (void);
+void program_no_7 (void);
+void program_no_8 (void);
+
+// main program
+int main(int argc, char const *argv[])
+{
+    int selection = atoi(argv[1]); 
+    switch (selection)
+    {
+    case 1:
+        program_no_1();
+        break;
+    case 2:
+        program_no_2();
+        break;
+    case 3:
+        program_no_3();
+        break;
+    case 4:
+        program_no_4();
+        break;
+    case 5:
+        program_no_5();
+        break;
+    case 6:
+        program_no_6();
+        break;
+    case 7:
+        program_no_7();
+        break;
+    case 8:
+        program_no_8();
+        break;
+    default:
+        printf("Usage: ./repetition number\nExample: ./repetition 1\n");
+        break;
+    }
+    return 0;
+}
 
 // Program no 1
-int main(void)
+void program_no_1 (void)
 {
     int i, x;
     int fx = 0;
@@ -11,16 +59,15 @@ int main(void)
         fx += (i*2) + fx;
     }
     printf("nilai f(x) = %i\n", fx);
-    return 0;
 }
 
 // program no 2 
 /* merupakan program untuk melakukan validasi angka dari inputan pengguna */
-void main(void)
+void program_no_2 (void)
 {
     int number; 
     int trials = 0;
-    printf("Masukkan angka: "); scanf("%i", &number);
+    printf("Masukkan angka:\n "); scanf("%i", &number);
 
     while (number < 0)
     {
@@ -33,27 +80,29 @@ void main(void)
 
 // percobaan no 3
 /* program menebak angka, akan mengulang 3 kali dan print hasil akhir */
-int main(void)
+void program_no_3 (void)
 {   
     const int angkaRahasia = 10;
-    int tebakan;
-    for (int i = 0; i < 3; i++)
+    int tebakan, i = 0;
+    do
     {
-        printf("tebakan ke-%i: "); scanf("%i", &tebakan);
-        scanf("%i", &tebakan);
-
-        if (tebakan == angkaRahasia)
-        {
-            printf("Anda Menang\n");
-            return 0;
-        }
+        printf("tebakan ke-%i: ", i+1); scanf("%i", &tebakan);
+        i++;
     }
-    printf("Anda Kalah\n");
-    return 0;
+    while (tebakan != angkaRahasia && i < 3);
+    
+    if (tebakan == angkaRahasia && i < 3)
+    {
+        printf("Anda Menang\n");
+    }
+    else
+    {
+        printf("Anda Kalah\n");
+    }
 }
 
-// percobaan no 4 & 7
-int main(void)
+// percobaan no 4 | for increment
+void program_no_4 (void)
 {
     int x, a = 1;
     printf("Masukkan faktorial: "); scanf("%i", &x);
@@ -61,18 +110,11 @@ int main(void)
     {
         a *= i;
     }
-
-    for (int i = 1; i <= x; x++) /* menggunakan for menurun */
-    {
-        a *= x;
-    }
-    printf("nilai faktorial: % i\n", a);
-    return 0;
 }
 
 // percobaan no 5
 /* membuat segitiga */
-int main(void)
+void program_no_5 (void)
 {
     char simbol;
     int x;
@@ -80,17 +122,16 @@ int main(void)
     printf("Masukkan tinggi: "); scanf("%i", &x);
     for (int i = 0; i < x; i++)
     {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j <= i; j++)
         {
             printf("%c", simbol);
         }
         printf("\n");
     }
-    return 0;
 }
 
-// percobaan no 6
-int main(void)
+// percobaan no 6 
+void program_no_6 (void)
 {
     int n, bil, total = 0;
     printf("Masukkan angka: "); scanf("%i", &n);
@@ -109,8 +150,20 @@ int main(void)
     printf("total: %i\n", total);
 }
 
+// program no 7 | menggunakan for menurun
+void program_no_7 (void) 
+{
+    int x, a = 1;
+    printf("Masukkan faktorial: "); scanf("%i", &x);
+    for (int i = 1; i <= x; x++) /* menggunakan for menurun */
+    {
+        a *= x;
+    }
+    printf("nilai faktorial: % i\n", a);
+}
+
 // percobaan 8 segitiga miring
-int main(void)
+void program_no_8 (void)
 {
     char simbol; 
     int x, i, j;
